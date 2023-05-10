4700ce42f1dd7649cf2c44de08dfb99bbe5e5c50
Smalick0478 <sameermalik0478@gmail.com>
2023-05-10 19:16:13 +0500

    Added Animation in Food

--- lib/food_pixel.dart
+++ lib/food_pixel.dart
@@ -1,27 +1,42 @@
-
 import 'package:flutter/material.dart';
 
-class FoodPixel extends StatelessWidget {
-  const FoodPixel({Key? key}) : super(key: key);
+class FoodPixel extends StatefulWidget {
+  @override
+  _AnimatedFoodState createState() => _AnimatedFoodState();
+}
 
+class _AnimatedFoodState extends State<FoodPixel>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _animation =
+        Tween<double>(begin: 1.0, end: 1.2).animate(_animationController);
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.yellowAccent,
-              Colors.lightGreenAccent
-            ],
-          ),
-          borderRadius: BorderRadius.circular(4),
+      child: ScaleTransition(
+        scale: _animation,
+        child: Image.asset(
+          'assets/food.png',
+          width: 24,
+          height: 24,
         ),
-        child: const Icon(Icons.fastfood_sharp),
       ),
     );
   }
