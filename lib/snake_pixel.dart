138048ab8653dceeaa949d024f6a0a0e9cd96dcb
Smalick0478 <sameermalik0478@gmail.com>
2023-05-10 19:16:42 +0500

    Snake Color Changed

--- lib/snake_pixel.dart
+++ lib/snake_pixel.dart
@@ -3,24 +3,21 @@ import 'package:flutter/material.dart';
 class SnakePixel extends StatelessWidget {
   const SnakePixel({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(2.0),
       child: Container(
-        decoration:  BoxDecoration(
+        decoration: BoxDecoration(
           gradient: const LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-              colors: [
-                Colors.pink,
-                Colors.pinkAccent,
+            colors: [
+              Colors.black,
+              Colors.black,
             ],
           ),
-          borderRadius: BorderRadius.circular(4),
+          borderRadius: BorderRadius.circular(400),
         ),
       ),
     );
